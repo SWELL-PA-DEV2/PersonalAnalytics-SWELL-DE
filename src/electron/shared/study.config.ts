b3dc7c53f4100a2d7d6ae10bcdd3429647eb0654
André Meyer <casaout@users.noreply.github.com>
2025-02-24 13:45:25 +0100

    temporary change in study.config.ts to merge from PA-main

--- src/electron/shared/study.config.ts
+++ src/electron/shared/study.config.ts
@@ -1,13 +1,14 @@
 import { StudyConfiguration } from './StudyConfiguration';
 
 const studyConfig: StudyConfiguration = {
-  name: 'SWELL - Student Wellbeing and Learning on Laptops',
-  shortDescription: 'tbd',
-  infoUrl: 'https://mydata-lab.uzh.ch/de/studien/SWELL.html',
-  privacyPolicyUrl: 'https://mydata-lab.uzh.ch/de/studien/SWELL.html',
-  uploadUrl: 'https://hasel.dev/swell-upload',
-  contactName: 'Dr. Malte Doehne, Andreas Baumer, Dr. Andre Meyer',
-  contactEmail: 'swell@d2usp.ch',
+  name: 'PersonalAnalytics Study',
+  shortDescription:
+    'PersonalAnalytics is a self-monitoring software developed by the Human Aspects of Software Engineering Lab of the University of Zurich to non-intrusively collect computer interaction data and store it locally on your computer. Every now and then, a self-reflection question asks you about time well spent and perceived productivity. In the future, it will add a retrospection that will visualize and correlate the automatically collected and manually reported data to help you learn more about how you spend your time and your productivity. This software is open source, can be adapted and re-used for your own scientific studies.',
+  infoUrl: 'https://github.com/HASEL-UZH/PersonalAnalytics',
+  privacyPolicyUrl: 'https://github.com/HASEL-UZH/PersonalAnalytics/blob/dev-am/documentation/PRIVACY.md',
+  uploadUrl: 'https://hasel.dev/upload',
+  contactName: 'Dr. André Meyer',
+  contactEmail: 'study@hasel.dev',
   subjectIdLength: 6,
   dataExportEnabled: true,
   dataExportEncrypted: false,
